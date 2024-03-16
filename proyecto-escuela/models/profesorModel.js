6f60d51757dac7bf22d1beb97f58ab9430699abf
matias199130 <matiasromero20121991@gmail.com>
2024-03-16 18:39:55 -0300

    incognita

---
+++ proyecto-escuela/models/profesorModel.js
@@ -0,0 +1,10 @@
+function patito(){
+    if(lolcito ===  undefined){
+        var lolcito = {
+            cito : function (texto, autor) {
+                return `Cita: "${text}" - ${autor}`;
+        }
+    }
+};
+    return console.log("hola ");
+};
